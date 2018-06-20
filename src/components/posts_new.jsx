34c864920b40c0dde4f1e78ea4c3af91dda2399a
Brian Bycynski <high5bri@hotmail.com>
2018-06-20 12:25:15 -0400

    finished lecture 136

---
+++ src/components/posts_new.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label htmlFor="">{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <form action="">
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a Title';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter a Category';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter Some Content';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+import './styles/index.css';
+
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+
+import registerServiceWorker from './registerServiceWorker';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route exact path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
